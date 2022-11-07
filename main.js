1113b4af20f951a560bd7fc4081aadeff4db405d
Amandeep Kumar <amankumaradk@gmail.com>
2022-11-07 18:15:33 +0530

    updated

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-  
+$(".owl-carousel").owlCarousel({
+  items: 4,
+  loop: true,
+  nav: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+  },
+});
